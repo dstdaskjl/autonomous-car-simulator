7ac94c49510b60805065b5d1d5b4a89fb2959326
dstdaskjl <55066249+dstdaskjl@users.noreply.github.com>
2022-04-28 03:48:40 -0400

    Added backup conditions

--- source/simulator.py
+++ source/simulator.py
@@ -8,6 +8,7 @@ from kivy.properties import NumericProperty, ListProperty, ReferenceListProperty
 from kivy.vector import Vector
 import random
 import math
+import time
 
 
 ANGLE_RANGE_WIDE = (100, 130)
@@ -18,7 +19,7 @@ DURATION = 0.3
 class Arena(Widget):
     def bounce(self, car):
         if not car.updating:
-            lhit, rhit = self.collide(car=car)
+            lhit, rhit = self.collide(obj=car)
             if lhit or rhit:
                 car.update_direction()
                 Clock.schedule_once(
@@ -27,45 +28,45 @@ class Arena(Widget):
                 )
 
     def safe_angle(self, lhit, rhit):
-        if rhit and lhit:
+        if lhit and rhit:
             return random.randint(*ANGLE_RANGE_WIDE) * random.choice([-1, 1])
-        elif rhit:
-            return random.randint(*ANGLE_RANGE_NARROW)
-        else:
+        elif lhit:
             return -random.randint(*ANGLE_RANGE_NARROW)
+        else:
+            return random.randint(*ANGLE_RANGE_NARROW)
 
-    def collide(self, car):
-        lhit = not self.collide_point(*car.abs_left())
-        rhit = not self.collide_point(*car.abs_right())
+    def collide(self, obj):
+        lhit = not self.collide_point(*obj.abs_left())
+        rhit = not self.collide_point(*obj.abs_right())
         return lhit, rhit
 
 
 class Maze(GridLayout):
     obstacles = ListProperty()
 
-    def bounce(self, car):
-        if not car.updating:
-            lhit, rhit = self.collide(car=car)
+    def bounce(self, obj):
+        if not obj.updating:
+            lhit, rhit = self.collide(obj=obj)
             if lhit or rhit:
-                car.update_direction()
+                obj.update_direction()
                 Clock.schedule_once(
-                    callback=lambda *args: car.rotate(angle=self.safe_angle(lhit=lhit, rhit=rhit)),
+                    callback=lambda *args: obj.rotate(angle=self.safe_angle(lhit=lhit, rhit=rhit)),
                     timeout=DURATION
                 )
 
     def safe_angle(self, lhit, rhit):
-        if rhit and lhit:
+        if lhit and rhit:
             return random.randint(*ANGLE_RANGE_WIDE) * random.choice([-1, 1])
-        elif rhit:
-            return random.randint(*ANGLE_RANGE_NARROW)
-        else:
+        elif lhit:
             return -random.randint(*ANGLE_RANGE_NARROW)
+        else:
+            return random.randint(*ANGLE_RANGE_NARROW)
 
-    def collide(self, car):
+    def collide(self, obj):
         for obstacle in self.obstacles:
-            rhit = obstacle.collide_point(*car.abs_right())
-            lhit = obstacle.collide_point(*car.abs_left())
-            if rhit or lhit:
+            lhit = obstacle.collide_point(*obj.abs_left())
+            rhit = obstacle.collide_point(*obj.abs_right())
+            if lhit or rhit:
                 return lhit, rhit
         return None, None
 
@@ -93,6 +94,11 @@ class Car(Widget):
     velocity_y = NumericProperty(0)
     velocity = ReferenceListProperty(velocity_x, velocity_y)
 
+    arena = ObjectProperty(None)
+    maze = ObjectProperty(None)
+    lwing = ObjectProperty(None)
+    ring = ObjectProperty(None)
+
     def __init__(self, **kwargs):
         super(Car, self).__init__(**kwargs)
         self.updating = False
@@ -105,14 +111,36 @@ class Car(Widget):
         self.pos = -Vector(*self.velocity) + self.pos
 
     def rotate(self, angle):
-        self.update_flag(val=True)
-        self.update_vector(angle=angle)
-        self.update_direction()
-        self.update_arrow(direction='left' if angle > 0 else 'right')
-        updated_angle = self.angle + angle
-        anim = Animation(angle=updated_angle, duration=DURATION)
-        anim.bind(on_complete=lambda *args: self.update_flag(val=False))
-        anim.start(widget=self)
+        lal, lar = self.arena.collide(self.lwing)
+        ral, rar = self.arena.collide(self.rwing)
+        lml, lmr = self.maze.collide(self.lwing)
+        rml, rmr = self.maze.collide(self.rwing)
+
+        if lal or lar or ral or rar or lml or lmr or rml or rmr:
+            if ((lal or lar) and (ral or rar)) or ((lml or lmr) and (rml or rmr)):
+                Clock.schedule_once(
+                    callback=lambda *args: self.rotate(angle=abs(angle) * random.choice([-1, 1])),
+                    timeout=DURATION * 3
+                )
+            elif lal or lar or lml or lmr:
+                Clock.schedule_once(
+                    callback=lambda *args: self.rotate(angle=abs(angle) * -1),
+                    timeout=DURATION * 3
+                )
+            else:
+                Clock.schedule_once(
+                    callback=lambda *args: self.rotate(angle=abs(angle)),
+                    timeout=DURATION * 3
+                )
+        else:
+            self.update_flag(val=True)
+            self.update_vector(angle=angle)
+            self.update_direction()
+            self.update_arrow(direction='left' if angle > 0 else 'right')
+            updated_angle = self.angle + angle
+            anim = Animation(angle=updated_angle, duration=DURATION)
+            anim.bind(on_complete=lambda *args: self.update_flag(val=False))
+            anim.start(widget=self)
 
     def reset_arrow(self):
         self.arrow = 'None'
@@ -155,6 +183,46 @@ class Head(Widget):
         anim.start(widget=self)
 
 
+class LWing(Widget):
+    car = ObjectProperty(None)
+
+    def abs_left(self):
+        x, y = self.x + self.width, self.y
+        cx, cy = self.car.center_x, self.car.center_y
+        a = math.radians(self.car.angle)
+        nx = ((x - cx) * math.cos(a)) - ((y - cy) * math.sin(a)) + cx
+        ny = ((x - cx) * math.sin(a)) + ((y - cy) * math.cos(a)) + cy
+        return nx, ny
+
+    def abs_right(self):
+        x, y = self.x, self.y
+        cx, cy = self.car.center_x, self.car.center_y
+        a = math.radians(self.car.angle)
+        nx = ((x - cx) * math.cos(a)) - ((y - cy) * math.sin(a)) + cx
+        ny = ((x - cx) * math.sin(a)) + ((y - cy) * math.cos(a)) + cy
+        return nx, ny
+
+
+class RWing(Widget):
+    car = ObjectProperty(None)
+
+    def abs_left(self):
+        x, y = self.x, self.top
+        cx, cy = self.car.center_x, self.car.center_y
+        a = math.radians(self.car.angle)
+        nx = ((x - cx) * math.cos(a)) - ((y - cy) * math.sin(a)) + cx
+        ny = ((x - cx) * math.sin(a)) + ((y - cy) * math.cos(a)) + cy
+        return nx, ny
+
+    def abs_right(self):
+        x, y = self.x + self.width, self.top
+        cx, cy = self.car.center_x, self.car.center_y
+        a = math.radians(self.car.angle)
+        nx = ((x - cx) * math.cos(a)) - ((y - cy) * math.sin(a)) + cx
+        ny = ((x - cx) * math.sin(a)) + ((y - cy) * math.cos(a)) + cy
+        return nx, ny
+
+
 class Obstacle(Widget):
     color = ListProperty((0.1, 0.2, 0.3, 1))
 
@@ -172,6 +240,13 @@ class Simulator(Screen):
     def start(self, vel=(-4, 0)):
         self.car.center = self.center
         self.car.velocity = vel
+        self.car.arena = self.arena
+        self.car.maze = self.maze
+        self.car.lwing = self.lwing
+        self.car.rwing = self.rwing
+        self.lwing.car = self.car
+        self.rwing.car = self.car
+
         self.maze.add_obstacles()
         Clock.schedule_interval(callback=self.update, timeout=1/30)
         self.head.rotate()
